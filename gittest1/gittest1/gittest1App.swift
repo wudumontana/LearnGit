44c7b34e925382ad0004e2b32c28145a0994c2eb
杨泞滔 <yangningtao@baodeMacBook-Pro.local>
2021-11-28 19:16:05 +0800

    LearnGit

---
+++ gittest1/gittest1/gittest1App.swift
@@ -0,0 +1,17 @@
+//
+//  gittest1App.swift
+//  gittest1
+//
+//  Created by 杨泞滔 on 2021/11/28.
+//
+
+import SwiftUI
+
+@main
+struct gittest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
